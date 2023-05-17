073ec9ea44ad539ec86f0042b8e11e6a80773c60
Logan Dailey <logan.l.dailey.25@dartmouth.edu>
2023-05-17 09:21:59 -0400

    player updates

--- player/player.c
+++ player/player.c
@@ -16,16 +16,39 @@
 typedef struct player {
   grid_t* grid;       // personal map of what they can see
   char c;               //what character they are
+  char* name;           //what they say their name is
   int score;            //current score
   int x;                //location
   int y;                //location
   bool active;          //still in or has quit?
 } player_t;
 
-player_t* player_new(char c, char* map) {                               //note: should they get a grid loaded in?
+player_t* player_new(char c, char* name) {                               //note: should they get a grid loaded in?
 
     player_t* player = mem_mallocc(sizeof(player_t));
+    player->c = c;
+    player->name = name;
+    score = 0;
+    x = 0;
+    y = 0;
+    active = true;                                                      //note should not start active yet?
+    
+}
 
-    player->grid = grid_new(NR, NC)                                     //note: should these get passed in or dimensions inferred?
-    grid_load(map);
+void player_load_map(player_t* player, char* map) {
+
+  player->grid = grid_new(NR, NC)                                     //note: should these get passed in or dimensions inferred?
+  grid_load(map);
+
+}
+
+void player_deactivate(player_t* player) {
+
+  if (player->active == true) {
+    active = false;
+    return;
+  }
+
+  if (player->active )
+
 }
