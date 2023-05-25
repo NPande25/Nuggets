6fc5de2897ae6da2e5e2c49f4363c0f647c748b2
Nikhil Pande <nikhil.a.pande.25@dartmouth.edu>
2023-05-25 18:14:11 -0400

    gridcell functional, documentation in progress

--- player/gridcell.c
+++ player/gridcell.c
@@ -1,5 +1,11 @@
 /* 
+ * CS50 NUggets Project
+ * Team 17 - CecsC
+ * 
  * gridcell.c - CS50 'gridcell' module
+ * The gridcell module defines each individual piece of the gridcell,
+ * storing its character (wall, blank, gold, etc), its location, and
+ * whether or not it can be seen 
  *
  * see gridcell.h for more information.
  *
@@ -11,7 +17,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include "player.h"
+#include "mem.h"
 #include "grid.h"
+#include "gridcell.h"
 
 /**************** file-local global variables ****************/
 /* none */
@@ -30,7 +38,7 @@ typedef struct gridcell {
 
 gridcell_t* gridcell_new(char c, int x, int y, int gold, bool show) 
 {
-  gridcell_t* gridcell = mem_malloc(sizeof(gridcell_t));
+  gridcell_t* gridcell = mem_assert(malloc(sizeof(gridcell_t)), "Gridcell allocation error");
   
   gridcell->c = c;
   gridcell->x = x;
@@ -38,14 +46,55 @@ gridcell_t* gridcell_new(char c, int x, int y, int gold, bool show)
   gridcell->gold = gold;
   gridcell->show = show;
 
+  return gridcell;
+
 }
+
+
+void gridcell_set(gridcell_t* gridcell, char c)
+{
+  if (gridcell == NULL) {
+    fprintf(stderr, "gridcell null in gridcell_set");
+  }
+
+  gridcell->c = c;
+}
+
+
+int gridcell_getX(gridcell_t* gridcell)
+{
+  if (gridcell == NULL) {
+    fprintf(stderr, "gridcell null in gridcell_getX");
+  }
+
+  return gridcell->x;
+}
+
+int gridcell_getY(gridcell_t* gridcell)
+{
+  if (gridcell == NULL) {
+    fprintf(stderr, "gridcell null in gridcell_getY");
+  }
+
+  return gridcell->y;
+}
+
+
+void gridcell_print(gridcell_t* gridcell)
+{
+  if (gridcell == NULL) {
+    fprintf(stderr, "gridcell null in gridcell_print");
+  }
+
+  printf("%c\n", gridcell->c);
+}
+
 
 void gridcell_delete(gridcell_t* gridcell)
 {
-  if (gridcell != NULL) {
+  if (gridcell == NULL) {
     fprintf(stderr, "Null gridcell to delete");
   } else {
     free(gridcell);
   }
 }
-
---
+++ player/gridcell.h
@@ -0,0 +1,20 @@
+/*
+ * CS50 Nuggets Project
+ * Team 17 - CecsC
+ * 
+ * gridcell.h - header file for gridcell.c
+ * 
+ */
+
+// Gridcell struct
+typedef struct gridcell gridcell_t;
+
+// functions
+gridcell_t* gridcell_new(char c, int x, int y, int gold, bool show);
+void gridcell_set(gridcell_t* gridcell, char c);
+int gridcell_getX(gridcell_t* gridcell);
+int gridcell_getY(gridcell_t* gridcell);
+void gridcell_print(gridcell_t* gridcell)
+void gridcell_delete(gridcell_t* gridcell);
+
+
